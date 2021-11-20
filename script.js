e02d139b5e5163e7a287a6ff158c077c8f24180b
BeeCee <68548366+HolyBaby023@users.noreply.github.com>
2021-11-20 19:18:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector('.burger')
+const x = document.querySelector('.x')
+const navLinks = document.querySelector('.nav-links')
+const footerDiscord = document.querySelector('.footer-discord')
+
+burger.addEventListener('click', function() {
+    x.classList.toggle('x-show')
+    burger.classList.toggle('burger-hide')
+    navLinks.classList.add('nav-ul-show')
+})
+
+x.addEventListener('click', function() {
+    x.classList.toggle('x-show')
+    burger.classList.toggle('burger-hide')
+    navLinks.classList.toggle('nav-ul-show')
+})
+
+footerDiscord.addEventListener('click')
